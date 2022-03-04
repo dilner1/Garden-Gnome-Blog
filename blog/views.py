8a4f6d22a6f594aec3267c7d1701218f602e07f4
dilner1 <dan.milner871@gmail.com>
2022-03-04 16:27:38 +0000

    updating template name path in views.py

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,8 @@ from .models import Post
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
+    paginate_by = 8
 
 class PostDetail(generic.DetailView):
     model = Post
-    template_name = 'post_detail.html'
+    template_name = 'open_post.html'
