4b6f7e85913d09acab98ed2a589983aa0df72746
dilner1 <dan.milner871@gmail.com>
2022-03-05 02:58:34 +0000

    Showing post details on open_post.html

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from . import views
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='open_post'),
+    
 ]
