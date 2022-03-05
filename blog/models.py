110c2bc9ff6b8de1debec1f6345777021899ab7e
dilner1 <dan.milner871@gmail.com>
2022-03-05 04:55:53 +0000

    removing featured_image from models.py as breaking

--- blog/models.py
+++ blog/models.py
@@ -11,6 +11,7 @@ class Post(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
     updated_on = models.DateTimeField(auto_now= True)
+    # featured_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
