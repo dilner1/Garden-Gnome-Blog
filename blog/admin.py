1dbb83f503bbb7ff4481cbf52219d67418d4f1ee
dilner1 <dan.milner871@gmail.com>
2022-03-05 00:55:11 +0000

    Implementing sumernote to admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from .models import Post
+from django_summernote.admin import SummernoteModelAdmin
 
 # Additional information for Posts in admin dashboard
 
-class PostAdmin(admin.ModelAdmin):
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    search_fields = ('title', 'content')
     list_display = ('title', 'slug', 'status', 'created_on')
     list_filter = ('status', 'created_on')
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
 
-admin.site.register(Post, PostAdmin)
+# admin.site.register(Post, PostAdmin)
