f896db086a967cba90158233a4b8405fd52dfa40
dilner1 <dan.milner871@gmail.com>
2022-03-11 14:30:25 +0000

    reverting fields in views.py to change status

--- blog/views.py
+++ blog/views.py
@@ -16,5 +16,4 @@ class PostDetail(generic.DetailView):
 class AddPost(generic.CreateView):
     model = Post
     template_name = 'add_post.html'
-    # fields = ('title', 'slug', 'author', 'image', 'content', 'status')
-    fields = ('title', 'slug', 'author', 'image', 'content')
+    fields = ('title', 'slug', 'author', 'image', 'content', 'status')
