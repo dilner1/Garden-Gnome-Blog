2f34961ba0bd02ab72d36bb714cc4dc3440ea6d9
dilner1 <dan.milner871@gmail.com>
2022-03-15 17:16:19 +0000

    Adding cripsy forms to allauth accounts

--- gardengnome/settings.py
+++ gardengnome/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.sites',
     'allauth',
+    'allauth.account',
     'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
