3666c03d1966f3e1cfdd5f221486f48fe06fe841
dilner1 <dan.milner871@gmail.com>
2022-03-21 22:45:37 +0000

    Adding time since post created on comments

--- blog/models.py
+++ blog/models.py
@@ -49,4 +49,4 @@ class Comment(models.Model):
         ordering = ['created_on']
 
     def __str__(self):
-        return f'Comment {self.body} by {self.name}'
+        return f'{self.name}: {self.body}'
